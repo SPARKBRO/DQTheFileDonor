4f571290ffdce2e495dab5fc2737a633dad79a5f
SPARKBRO <103011794+SPARKBRO@users.noreply.github.com>
2023-01-14 01:18:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,9 +21,9 @@ BOT_TOKEN = environ['BOT_TOKEN']
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/6a0726f79acd8300e9a04.jpg https://telegra.ph/file/68289fefb76dbc43b766d.jpg https://telegra.ph/file/0caad29c0cf91c23fb1b6.jpg https://telegra.ph/file/8c34c755dd16581c1c6b5.jpg https://telegra.ph/file/365e35b554e5a3ea83857.jpg https://telegra.ph/file/07f185825c5b7bfd6fbfb.jpg https://telegra.ph/file/85f95494565a762edb3e7.jpg https://telegra.ph/file/708a1d6ce805fcc6a46d0.jpg https://telegra.ph/file/d799c1a964f211028cc97.jpg https://telegra.ph/file/b987425b80bca0cf45c7e.jpg https://telegra.ph/file/2a8b3779760289b76de24.jpg https://telegra.ph/file/47961be968719b3e24cf0.jpg https://telegra.ph/file/2e127b0f6b1810d733c09.jpg https://telegra.ph/file/281b18770a43a29120252.jpg https://telegra.ph/file/2086dd2aa8382e758a599.jpg https://telegra.ph/file/fcc849db4bf5c517f0f8d.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/e8173734a0d23b8a42152.jpg https://telegra.ph/file/b859487dfee1da0c3825e.jpg https://telegra.ph/file/22dfda0f771ffb37854f6.jpg https://telegra.ph/file/5f9961fb8a1d5b6c2ac58.jpg https://telegra.ph/file/41005ef88128a7bf3c1a0.jpg https://telegra.ph/file/f0cfa80db915f5d6f5958.jpg https://telegra.ph/file/3a30781b745ed04dfae14.jpg https://telegra.ph/file/4ac62047ca06b750a8647.jpg https://telegra.ph/file/ba385c1fcda76245947a6.jpg https://telegra.ph/file/0df0723840ef1a56c98f6.jpg https://telegra.ph/file/fc65f6292a1de26b4a035.jpg https://telegra.ph/file/fa8600aec22b6e81d532d.jpg https://telegra.ph/file/19dd05c56cc83b3b23419.jpg https://telegra.ph/file/6f0277517a47643f2d226.jpg https://telegra.ph/file/3ddec577fe00b45fdd80e.jpg https://telegra.ph/file/ff8f2c5775e571765d24f.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/ffb79ee7cc328a69ce1e0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
 
 # Admins, Channels & Users
@@ -49,8 +49,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+GOFte-Rz2tcxODg1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+mCMdCb_ymAowZmNl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Moviemania_Request')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Moviemania_TG')
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'DQ_The_File_Donor_Support')
